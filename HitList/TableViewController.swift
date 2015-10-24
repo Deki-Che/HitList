e9906eb01afd60ad47828482f76fd269166b1053
dekich <794481112@qq.com>
2015-10-24 19:38:09 +0800

    实现了基本的存取

--- HitList/TableViewController.swift
+++ HitList/TableViewController.swift
@@ -7,19 +7,23 @@
 //
 
 import UIKit
+import CoreData
 
 class TableViewController: UITableViewController {
     // 保存姓名数组
     var names = [String]()
+    var people = [NSManagedObject]()
 
     @IBAction func addName(sender: AnyObject) {
         let alertController = UIAlertController(title: "添加新姓名", message: "请输入姓名", preferredStyle: UIAlertControllerStyle.Alert)
         let alertActionDone = UIAlertAction(title: "保存", style: UIAlertActionStyle.Default) {
             (action: UIAlertAction!) -> Void in
             let textField = alertController.textFields![0] as UITextField
-            self.names.append(textField.text!)
+            
+            // 保存数据coredata
+            self.saveName(textField.text!)
             //self.tableView.reloadData()
-            let indexPath = NSIndexPath(forRow: (self.names.count - 1), inSection: 0)
+            let indexPath = NSIndexPath(forRow: (self.people.count - 1), inSection: 0)
             self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
             
             
@@ -32,10 +36,56 @@ class TableViewController: UITableViewController {
 
         self.presentViewController(alertController, animated: true, completion: nil)
     }
+    
+    func saveName(text:String) {
+        // 1 取得总代理 ，和总管对象
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        // 内容总管
+        let managedObjectContext = appDelegate.managedObjectContext
+        
+        // 2 建立一个 entity
+        let entity = NSEntityDescription.entityForName("Person", inManagedObjectContext: managedObjectContext)
+        
+        // 创建单个人
+        let person = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedObjectContext)
+        
+        // 3 person 保存传过来的数据
+        person.setValue(text, forKey: "name")
+        
+        // 4保存entity 到托管对象总管, 需要捕获错误
+        do {
+            try managedObjectContext.save()
+            people.append(person)
+        }catch let erro as NSError {
+            print("不能保存\(erro), \(erro.userInfo)")
+        }
+        
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "姓名列表"
-
+        
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        // 取得代理总管
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        let manageObjectContext = appDelegate.managedObjectContext
+        
+        // 获取的请求
+        let fetchRequst = NSFetchRequest(entityName: "Person")
+        
+        // 执行请求
+        do {
+            let results = try manageObjectContext.executeFetchRequest(fetchRequst)
+            people = results as! [NSManagedObject]
+            self.tableView.reloadData()
+        } catch let erro as NSError {
+            print("不能捕获 \(erro), \(erro.userInfo)")
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -51,7 +101,7 @@ class TableViewController: UITableViewController {
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  names.count
+        return  people.count
     }
 
     
@@ -59,7 +109,7 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
 
         // Configure the cell...
-        cell.textLabel?.text = names[indexPath.row]
+        cell.textLabel?.text = people[indexPath.row].valueForKey("name") as? String
 
         return cell
     }
